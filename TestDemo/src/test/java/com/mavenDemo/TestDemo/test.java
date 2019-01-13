c8ddd4301d5a1ac7a6e2473bddba6349443d69d0
VickyJoy <46643891+VickyJoy@users.noreply.github.com>
2019-01-13 19:21:59 +0800

    update testdome

---
+++ TestDemo/src/test/java/com/mavenDemo/TestDemo/test.java
@@ -0,0 +1,11 @@
+package com.mavenDemo.TestDemo;
+import static io.restassured.RestAssured.*;
+//import static io.restassured.matcher.ResponseAwareMatcher.*;
+//import static org.hamcrest.Matchers.*;
+public class test {
+ public static void main(String[]args){
+	 get("https://testerhome.com");
+	 
+ }
+ 
+}
